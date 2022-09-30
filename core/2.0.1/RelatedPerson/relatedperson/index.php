c62c71e7bf5b1c525963829450e5d2f41d47845b
costateixeira <costateixeira@users.noreply.github.com>
2022-09-30 20:06:11 +0000

    deploy: 9b0d26c857fe1fcd4c975b53c146c42b8b5ea7fe

---
+++ core/2.0.1/RelatedPerson/relatedperson/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.html');
+else 
+  Redirect('https://www.ehealth.fgov.be/standards/fhir/core/2.0.1/RelatedPerson-relatedperson.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
